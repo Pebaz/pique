c7256229e2b8b00314d96f40160e6daa1577568b
Pebaz <pebazium@gmail.com>
2020-02-27 11:42:54 -0500

    first version of pique

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::io::{self, Read};
+use json;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+    let mut buffer = String::new();
+
+    if let Ok(_) = handle.read_to_string(&mut buffer) {
+        if let Ok(object) = json::parse(&mut buffer) {
+            let mut result = &object;
+
+            for arg in env::args().skip(1) {
+                result = &result[&arg];
+            }
+    
+            println!("{}", result);
+        }
+
+        else {
+            println!("Error parsing JSON input");
+        }
+    }
 }
