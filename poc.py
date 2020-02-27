af65bbd2f18a4b27426b6d88c56ca37dfbef94f7
Pebaz <pebazium@gmail.com>
2020-02-27 16:58:08 -0500

    working poc

--- poc.py
+++ poc.py
@@ -3,12 +3,28 @@
 
 import sys, json
 
+def parse_commands(string):
+    cmds = []
+    for cmd in string.split('.'):
+        if cmd.startswith('['):
+            cmd = cmd[1:-1]
+            if cmd[0] != '*':
+                cmd = int(cmd)
+        cmds.append(cmd)
+    return cmds
+
+def drilldown(data, commands):
+    for i in range(len(commands)):
+        cmd = commands[i]
+        if cmd == '*':
+            data = [drilldown(element, commands[i + 1:]) for element in data]
+            break
+        else:
+            data = data[cmd]
+    return data
+
+
 data = json.loads(sys.stdin.read())
-
-command = sys.argv[1:]
-
-result = data
-for key in command:
-    result = result[key]
+result = drilldown(data, parse_commands(sys.argv[1]))
 
 print(json.dumps(result, indent=4))
