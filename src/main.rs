d6200b9b91c979bcc4b02081988c06ac3262c7ef
Pebaz <pebazium@gmail.com>
2020-03-02 16:56:48 -0500

    finally got it to compile sheesh

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-#[macro_use] extern crate rustpython_vm;
-
+#[macro_use] extern crate rustpython_vm as rvm;
 use std::env;
 use std::io::{self, Read};
 use json;
@@ -33,5 +32,26 @@ fn main() {
         }
     }
 
+    // https://github.com/pickitup247/pyckitup/blob/master/src/main.rs
+    let mut vm = rvm::VirtualMachine::new();
+
+    let code_obj = rvm::compile::compile(
+        "print([i for i in range(10)])\n",
+        &rvm::compile::Mode::Exec,
+        "<embedded>".to_owned(),
+        vm.ctx.code_type()
+    ).unwrap();
     
+    let builtin = vm.get_builtin_scope();
+    let scope = vm.context().new_scope(Some(builtin));
+    let result = vm.run_code_obj(code_obj, scope.clone());
+    match result {
+        Ok(res) => {
+            println!("{}", res);
+        }
+
+        Err(py_err) => {
+            println!("{}", vm.to_pystr(&py_err).unwrap());
+        }
+    }
 }
