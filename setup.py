4de0a991898961e27b6e965bdc1751c8a927e583
Pebaz <pebazium@gmail.com>
2020-05-13 13:40:35 -0400

    bumping version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='python-pique',
-	version='0.0.1',
+	version='0.0.2',
 	license="MIT",
 	description='Process JSON from the CLI easily',
 	author='http://github.com/Pebaz',
