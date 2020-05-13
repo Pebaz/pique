147ae82ad44ce371162e91d3efd963097f7e5bcc
Pebaz <pebazium@gmail.com>
2020-05-13 13:44:48 -0400

    fixed bug in upload workflow

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='python-pique',
-	version='0.0.2',
+	version='0.1.0',
 	license="MIT",
 	description='Process JSON from the CLI easily',
 	author='http://github.com/Pebaz',
